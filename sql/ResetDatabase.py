c21fa838f840278708dc8b4a3666e9a63c1c0524
Owen Halliday <hallidayo@moravian.edu>
2025-02-21 16:05:27 -0500

    create reset script for DB

---
+++ sql/ResetDatabase.py
@@ -0,0 +1,47 @@
+from DropTables import drop_comments_table, drop_dockets_table, drop_documents_table
+from CreateTables import (
+    create_comments_table,
+    create_dockets_table,
+    create_documents_table,
+)
+from dotenv import load_dotenv
+import psycopg
+import sys
+import os
+
+
+def main():
+    load_dotenv()
+
+    dbname = os.getenv("POSTGRES_DB")
+    username = os.getenv("POSTGRES_USER")
+    password = os.getenv("POSTGRES_PASSWORD")
+    host = os.getenv("POSTGRES_HOST")
+    port = os.getenv("POSTGRES_PORT")
+
+    conn_params = {
+        "dbname": dbname,
+        "user": username,
+        "password": password,
+        "host": host,
+        "port": port,
+    }
+    try:
+        conn = psycopg.connect(**conn_params)
+    except psycopg.Error as e:
+        print(e)
+        sys.exit(1)
+
+    print("Dropping tables")
+    drop_comments_table(conn)
+    drop_documents_table(conn)
+    drop_dockets_table(conn)
+
+    print("\nRecreating tables...")
+    create_dockets_table(conn)
+    create_documents_table(conn)
+    create_comments_table(conn)
+
+
+if __name__ == "__main__":
+    main()
