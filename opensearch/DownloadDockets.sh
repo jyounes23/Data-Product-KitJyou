56b026cab5187330e8f845b871001daf53c1295f
Geoffrey Kleinberg <gkleinberg21@gmail.com>
2025-02-27 13:20:57 -0500

    Fixed typo in one of the docket IDs

--- opensearch/DownloadDockets.sh
+++ opensearch/DownloadDockets.sh
@@ -4,6 +4,6 @@ aws s3 sync s3://mirrulations/DEA/DEA-2016-0015/text-DEA-2016-0015/comments ./do
 aws s3 sync s3://mirrulations/HHS/HHS-OS-2018-0008/text-HHS-OS-2018-0008/comments ./docket-samples/HHS-OS-2018-0008/text-HHS-OS-2018-0008/comments
 aws s3 sync s3://mirrulations/FTC/FTC-2019-0054/text-FTC-2019-0054/comments ./docket-samples/FTC-2019-0054/text-FTC-2019-0054/comments
 aws s3 sync s3://mirrulations/FDA/FDA-2021-N-1088/text-FDA-2021-N-1088/comments ./docket-samples/FDA-2021-N-1088/text-FDA-2021-N-1088/comments
-aws s3 sync s3://mirrulations/HHS/HHS-OSR-2019-0007/text-HHS-OSR-2019-0007/comments ./docket-samples/HHS-OSR-2019-0007/text-HHS-OSR-2019-0007/comments
+aws s3 sync s3://mirrulations/HHS/HHS-OCR-2019-0007/text-HHS-OCR-2019-0007/comments ./docket-samples/HHS-OCR-2019-0007/text-HHS-OCR-2019-0007/comments
 aws s3 sync s3://mirrulations/HHS/HHS-OS-2019-0014/text-HHS-OS-2019-0014/comments ./docket-samples/HHS-OS-2019-0014/text-HHS-OS-2019-0014/comments
 
