1e641496b7478bf27e05fee814f15e115fa84b26
Yousuf Kanan <kanany@moravian.edu>
2025-02-26 20:57:18 -0500

    Add the Download Dockets Script

---
+++ opensearch/DownloadDockets.sh
@@ -0,0 +1,9 @@
+aws s3 sync s3://mirrulations/CMS/CMS-2012-0031 ./docket-samples/CMS-2012-0031
+aws s3 sync s3://mirrulations/HHS/HHS-OS-2016-0014 ./docket-samples/HHS-OS-2016-0014
+aws s3 sync s3://mirrulations/DEA/DEA-2016-0015 ./docket-samples/DEA-2016-0015
+aws s3 sync s3://mirrulations/HHS/HHS-OS-2018-0008 ./docket-samples/HHS-OS-2018-0008
+aws s3 sync s3://mirrulations/FTC/FTC-2019-0054 ./docket-samples/FTC-2019-0054
+aws s3 sync s3://mirrulations/FDA/FDA-2021-N-1088 ./docket-samples/FDA-2021-N-1088
+aws s3 sync s3://mirrulations/HHS/HHS-OSR-2019-0007 ./docket-samples/HHS-OSR-2019-0007
+aws s3 sync s3://mirrulations/HHS/HHS-OS-2019-0014 ./docket-samples/HHS-OS-2019-0014
+
