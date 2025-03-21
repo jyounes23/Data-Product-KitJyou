9f52d4355711c1bb4650b50fdde50312af7e44e5
Samuel Cadiz <cadizs@moravian.edu>
2025-03-21 13:42:34 -0400

    addressed date format

--- queries/query.py
+++ queries/query.py
@@ -6,8 +6,7 @@ import json
 import sys
 
 
-
-# Sort the list of json objects based on the given key
+# Sort the combined results based on the given sort_type
 def sort_aoss_results(results, sort_type, desc=True):
     """
     Sort a list of JSON objects based on the given sort_type.
@@ -35,28 +34,27 @@ def sort_aoss_results(results, sort_type, desc=True):
     # Validate sort_type
     valid_sort_types = {'dateModified', 'alphaByTitle', 'relevance'}
 
-    # Default to 'dateModified' if invalid
     if sort_type not in valid_sort_types:
         print("Invalid sort type. Defaulting to 'dateModified'")
         sort_type = 'dateModified'
 
-    # Sort based on the sort_type given
+
+    # Sort based on the sort_type
     if sort_type == 'dateModified':
 
         results.sort(
-            key=lambda x: datetime.strptime(
-                x.get('dateModified', '1900-01-01 00:00:00.000 +0000'),  # Default if missing
-                '%Y-%m-%d %H:%M:%S.%f %z'
+            key=lambda x: datetime.fromisoformat(
+                x.get('dateModified', '1900-01-01T00:00:00Z').replace('Z', '+00:00') 
             ), reverse=desc)
         
     elif sort_type == 'alphaByTitle':
         results.sort(key=lambda x: x.get('title', ''), reverse=not desc)
 
-    # Return sorted results as a JSON string for Lambda
+    # Return sorted results as a JSON string
     return json.dumps(results)
 
 
-
+# Query the OpenSearch API and append docket supplementary information
 def query(search_params): 
     os_results = query_OpenSearch(search_params.get('searchTerm'))
 
